6aa27ea1ba877effdfea0e9c0e218d1276f3d36e
Nobuyuki Takahiro <vivlio@kumihan.com>
2023-06-08 14:58:54 +0900

    ラムネ美容室　PC・SP版修正完了

--- assets/js/behavior.js
+++ assets/js/behavior.js
@@ -1,33 +1,33 @@
-// ////////////
-// // ローディング・アニメーション
-// function loaded() {
-//   const loading = document.getElementById("loading")
-//   loading.classList.remove("keep")
-// }
-// // ウィンドウを読み込んで2秒後には次に遷移する。
-// window.addEventListener('load', () => {
-//   setTimeout(loaded, 1500)
-// })
+////////////
+// ローディング・アニメーション
 
-// // behavior page top
-const toSectionLinkBtn = document.getElementById("to-section-link-btn")
-const contentsLinks = document.getElementById("contents-links")
-const linksLi = document.getElementById("contents-links").children
-// console.log(contentsLinks)
-// const mainNavMask = document.getElementById("main-nav-mask")
+function loaded() {
+  const loading = document.getElementById('loading')
+  loading.classList.remove('keep')
+}
+// ウィンドウを読み込んで2秒後には次に遷移する。
+window.addEventListener('load', () => {
+    setTimeout(loaded, 1500)
+  })
+    
+    
+////////////
+// ハンバーガー・メニュー
+const toSectionLinkBtn = document.getElementById('to-section-link-btn')
+const contentsLinks = document.getElementById('contents-links')
+const linksLi = document.getElementById('contents-links').children
 
 // メニューの切り替え
-toSectionLinkBtn.addEventListener("click", function () {
-  this.classList.toggle("active")
-  this.nextElementSibling.classList.toggle("appear")
+toSectionLinkBtn.addEventListener('click', function () {
+  this.classList.toggle('active')
+  this.nextElementSibling.classList.toggle('appear')
 })
 
-//////////////////////////////////// メニューをクリックしたら閉じるができてない。
-// ページ内スクロールの際に、メニューを閉じる
+// リンクをクリックでページ内スクロールの際にメニューを閉じる。
 Array.from(linksLi).forEach(el => {
   el.addEventListener('click', () => {
     toSectionLinkBtn.classList.remove('active')
-    contentsLinks.classList.remove("appear")
+    contentsLinks.classList.remove('appear')
   })
 })
 
@@ -36,8 +36,8 @@ Array.from(linksLi).forEach(el => {
 // one-after-another　次々に出現　横バージョン
 
 // end, scrub, onceについて
-      // start: "top 80%",
-      // end: "50% 50%",
+      // start: 'top 80%',
+      // end: '50% 50%',
       // scrub: .5,　これを設定する意図は、
 // endとscrubはセットで設定する。スクロールの『動き』に合わせたアニメーションを作りたいとき。
 // なのでこのコード以降全て削除している。
@@ -55,7 +55,7 @@ Array.from(linksLi).forEach(el => {
 //     gsap.from(elem, .7,{
 //       opacity: ...,
 //       delay: idx * 0.25,  // <= ここがかっこいいポイントだったけど。。。
-//       ease: "...",
+//       ease: '...',
 //       scrollTrigger: {
 //         trigger: elem,
 //         start: '...',
@@ -64,13 +64,12 @@ Array.from(linksLi).forEach(el => {
 //   })
 // })
 
-
-const oneAfterAnotherRow = document.querySelectorAll("#one-after-another-row")
+const oneAfterAnotherRow = document.querySelectorAll('#one-after-another-row')
 oneAfterAnotherRow.forEach(elem => {
   // 要素の子要素へアクセスはこれだけ。超絶賢い。
   gsap.from(elem.children, .7, {
     opacity: 0,
-    ease: "power3.easeOut",
+    ease: 'power3.easeOut',
     // ↓ 0.25秒ずつ延滞させて処理する。これだけ。
     stagger: .25,
     scrollTrigger: {
@@ -84,7 +83,7 @@ oneAfterAnotherRow.forEach(elem => {
 
 ////////////
 // one-after-another　次々に出現
-const oneAfterAnother = document.querySelectorAll("#one-after-another")
+const oneAfterAnother = document.querySelectorAll('#one-after-another')
 oneAfterAnother.forEach(elem => {
   gsap.from(elem.children, 1, {
     y: 100,
@@ -113,24 +112,25 @@ oneAfterAnother.forEach(elem => {
 // // はるかに『高速な動作』となる。
 
 // // 引数の意味
-// //    "beforebegin"  要素の直前に追加
-// //    "afterbegin"   要素の最初の子要素として追加
-// //    "beforeend"    要素の最後の子要素として追加
-// //    "afterend"     要素の直後に追加
+// //    'beforebegin'  要素の直前に追加
+// //    'afterbegin'   要素の最初の子要素として追加
+// //    'beforeend'    要素の最後の子要素として追加
+// //    'afterend'     要素の直後に追加
 
 // // 最初の子要素では単純にできる。では、最後の子要素ではどうするか。
 
-// var parentElement = document.getElementById("parent"); // 親要素の取得
+// var parentElement = document.getElementById('parent'); // 親要素の取得
 // var lastChildElement = parentElement.lastElementChild; // 最後の子要素の取得
 
 // // 最後の子要素の後ろに新たな要素を追加する
-// lastChildElement.insertAdjacentHTML("afterend", "<div>新たな要素</div>");
+// lastChildElement.insertAdjacentHTML('afterend', '<div>新たな要素</div>');
 
 // // 最後の子要素の前に新たな要素を追加する
-// lastChildElement.insertAdjacentHTML("beforebegin", "<div>新たな要素</div>");
+// lastChildElement.insertAdjacentHTML('beforebegin', '<div>新たな要素</div>');
 
 // // lastElementChildで取り込んでから処理するだけ。簡単な話。関数覚えようね。
 
+// insertAdjacentHTMLを使っているところ。カーテン効果のための下準備としてHTML構造に新たに要素を加える。
 // 動画にカーテンを敷設する。
 document.querySelectorAll('.media-wrapper').forEach(elem => {
   elem.insertAdjacentHTML('afterbegin', '<div class="curtains"></div>')
@@ -146,17 +146,11 @@ viewMoreFrame.forEach(elem => {
 })
 const curtainsWord = document.querySelectorAll('.curtains-word')
 
-// // insertAdjacentHTMLを使っているところ。カーテン効果のための下準備としてHTML構造を変更する。
-// document.querySelectorAll('.view-more .wrapper').forEach(elem => {
-//   elem.insertAdjacentHTML('afterbegin', '<div class="view-more-curtains"></div>')
-//   elem.insertAdjacentHTML('afterbegin', '<div class="over-green"><div class="frame">VIEW MORE</div></div>')
-// })
-
 ////////////
-// .curtains　カーテン効果
-const curtains = Array.from(document.querySelectorAll(".curtains"))
-const viewMoreCurtains = Array.from(document.querySelectorAll(".curtains-view-more"))
-const curtainsAll = curtains.concat(viewMoreCurtains)
+// .curtains　動画のカーテン効果
+const curtains = Array.from(document.querySelectorAll('.curtains'))
+const curtainsViewMore = Array.from(document.querySelectorAll('.curtains-view-more'))
+const curtainsAll = curtains.concat(curtainsViewMore)
 
 curtainsAll.forEach(elem => {
   gsap.to(elem, 1, {
@@ -164,22 +158,21 @@ curtainsAll.forEach(elem => {
     // これでそれぞれの幅に対して『duration: 1;』で適宜効果がついていく。
     // 色々な幅が混在するこんなところに『-2000』とかはない。動きが不自然だったのはそのせい。
     x: '-100%',
-    transformOrigin: "left top",
-    ease: "power1.easeOut",
+    transformOrigin: 'left top',
+    ease: 'power1.easeOut',
     scrollTrigger: {
       trigger: elem,
-      start: "top 80%",
+      start: 'top 80%',
       // markers: true,
     }
   })
 })
 
-
 ////////////
 // view more の文字のカーテン効果
-
 viewMoreFrame.forEach(elem => {
   elem.addEventListener('mouseenter', function () {
+    // curtainsWordは配列のはずなのに処理を通すのが凄いのかなんなのか。
     gsap.set(curtainsWord, {
       visibility: 'visible',
       // 緑の文字 左 → 右用
@@ -205,28 +198,28 @@ viewMoreFrame.forEach(elem => {
       // width: 'auto'
     })
   })
-
 })
 
 
 ////////////
 // .fade-in　徐々に現れる
-const stGadeIn = document.querySelectorAll(".fade-in")
+
+const stGadeIn = document.querySelectorAll('.fade-in')
 stGadeIn.forEach(elem => {
   gsap.from(elem, .5, {
     opacity: 0,
     scrollTrigger: {
       trigger: elem,
-      start: "top 80%",
+      start: 'top 80%',
       opacity: 1,
       // markers: true,
     }
   })
 })
 
+
 ////////////
 // 一文字ずつ現れる
-
 // ドキュメント上の該当文章を一文字ごとにバラバラにしていく。
 
 function splitText(className) {
@@ -265,7 +258,7 @@ preCharAll.forEach(elem => {
 ////////////
 // スタイル・モデルの写真リストの表示管理
 const models = Array.from(document.getElementById('style-sample').children)
-const previewModelBtn = document.querySelector(".preview-model-btn")
+const previewModelBtn = document.querySelector('.preview-model-btn')
 const numberOfPeople = models.length;
 const groupSize = Math.ceil(numberOfPeople / 12);
 const divideGroup = [];
@@ -303,7 +296,6 @@ const stylingViewModal = document.getElementById('styling-view-modal')
 const closedBtn = document.getElementById('closed-button')
 
 // swaiperのインスタンスを確保する。
-
 const modaleSwiper =  new Swiper('.swiper', {
   // 各属性
   // direction: 'horizon',
@@ -345,7 +337,6 @@ models.forEach((elem, idx) => {
     // インデックスn番目のスライドを呼び出している。
     // 必要であれば、関数の引数の最後にコールバック関数を入れる。
     modaleSwiper.slideTo(idx, 0)
-
     stylingViewModal.classList.add('style-fade-in')
     closedBtn.addEventListener('click', () => {
       stylingViewModal.classList.remove('style-fade-in')
@@ -355,22 +346,22 @@ models.forEach((elem, idx) => {
 
 
 ////////////
-// < div class="bgm-play-stop" >　音楽再生とiconの遷移
-const bgmPlayStop = document.getElementById("bgm-play-stop")
-const playIcon = document.getElementById("play-icon")
-const stopIcon = document.getElementById("stop-icon")
-const bgMusic = document.getElementById("bg-music")
+// < div class='bgm-play-stop' >　音楽再生とiconの遷移
+const bgmPlayStop = document.getElementById('bgm-play-stop')
+const playIcon = document.getElementById('play-icon')
+const stopIcon = document.getElementById('stop-icon')
+const bgMusic = document.getElementById('bg-music')
 let userAction = false
 
-bgmPlayStop.addEventListener("click", function() {
+bgmPlayStop.addEventListener('click', function() {
   if (bgMusic.paused) {
     bgMusic.play()
-    this.classList.remove("fa-volume-high")
-    this.classList.add("fa-volume-xmark")
+    this.classList.remove('fa-volume-high')
+    this.classList.add('fa-volume-xmark')
   } else {
     bgMusic.pause();
-    this.classList.add("fa-volume-high")
-    this.classList.remove("fa-volume-xmark")
+    this.classList.add('fa-volume-high')
+    this.classList.remove('fa-volume-xmark')
     bgMusic.currentTime = 0
   }
 })
@@ -382,9 +373,9 @@ bgmPlayStop.addEventListener("click", function() {
 //   arr.forEach(elem => {
 //     let lastChar = elem.textContent.slice(-1)
 //     let preText = elem.textContent.slice(0, -1)
-//     elem.innerHTML = `${preText}<span class="remove-letter-spacing">${lastChar}</span>`
+//     elem.innerHTML = `${preText}<span class='remove-letter-spacing'>${lastChar}</span>`
 //   })
 // }
 
-// const arg = document.querySelectorAll("arg")
+// const arg = document.querySelectorAll('arg')
 // killLetterSpace(arg)
